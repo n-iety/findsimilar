9816786390536c427412baf532a57046059bd804
n-iety <5123001b@g.hit-u.ac.jp>
2025-01-07 21:14:52 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def database_file(filename):
     return send_from_directory(app.config['DATABASE_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
